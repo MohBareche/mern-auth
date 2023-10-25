c82df85d8f0da773ad3def729f5b1954def1bed5
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2023-10-25 14:15:34 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,12 +19,7 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-app.use(
-    cors({
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    })
-);
+app.use(cors());
 app.use(cookieParser());
 
 app.use(express.json());
